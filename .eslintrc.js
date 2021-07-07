cdfd3e7cb7d387a8792b07d186f3a5f47af68936
mlvrkhn <martin.guide@gmail.com>
2021-07-07 14:44:52 +0200

    Silence eslint, remove boiler

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+        requireConfigFile: false
+    },
+    extends: ["@nuxtjs", "plugin:nuxt/recommended"],
+    plugins: [],
+    rules: {
+        semi: "off",
+        quotes: ["error", "double"],
+        indent: ["warn", 4]
+    }
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <Tutorial />
+    <div class="container">
+        <p>index</p>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
